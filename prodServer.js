ba5f352bf9da04fe1f5d4dbf62e67c6105024424
John Murphy <chiefsmurph@gmail.com>
2017-01-30 14:44:49 +0100

    prodServer port for heroku

--- prodServer.js
+++ prodServer.js
@@ -3,6 +3,8 @@ var app = express();
 
 app.use('/', express.static('dist'));
 
-app.listen(3000);
+var port = process.env.PORT || 3000;
 
-console.log('listening on 3000')
+app.listen(port);
+
+console.log('listening on ', port)
